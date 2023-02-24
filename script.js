ab018aeae88c0a26074b5f7b9de6fac467cafe51
Holdrulff <wesley.fernan1@gmail.com>
2023-02-23 22:53:19 -0300

    update

--- script.js
+++ script.js
@@ -1,47 +1,191 @@
 const canvasEl = document.querySelector("canvas"),
-  canvasCtx = canvasEl.getContext("2d");
+  canvasCtx = canvasEl.getContext("2d"),
+  gapX = 10;
 
-const lineWidth = 15;
+const mouse = { x: 0, y: 0 };
+
+const field = {
+  w: window.innerWidth,
+  h: window.innerHeight,
+  draw: function () {
+    canvasCtx.fillStyle = "#286047";
+    /*------------------------desenho do campo---------------------------------------*/
+    canvasCtx.fillRect(0, 0, this.w, this.h);
+    //a função .fillRect(x, y, width, height) desenha e define dimensões do retangulo
+  },
+};
+
+const line = {
+  w: 15,
+  h: field.h,
+  draw: function () {
+    canvasCtx.fillStyle = "#ffffff";
+    canvasCtx.fillRect(field.w / 2 - this.w / 2, 0, this.w, this.h);
+  },
+};
+
+const leftPaddle = {
+  x: gapX,
+  y: field.h / 2 + 200,
+  w: line.w,
+  h: 200,
+  _move: function () {
+    this.y = mouse.y - this.h / 2;
+  },
+  draw: function () {
+    canvasCtx.fillStyle = "#ffffff";
+    canvasCtx.fillRect(this.x, this.y, this.w, this.h);
+    this._move();
+  },
+};
+
+const rightPaddle = {
+  x: window.innerWidth - line.w - gapX,
+  y: field.h / 2 + 200,
+  w: line.w,
+  h: 200,
+  directionY: 1,
+  _move: function () {
+    this.y = ball.y - this.h / 2;
+  },
+  draw: function () {
+    canvasCtx.fillStyle = "#ffffff";
+    canvasCtx.fillRect(this.x, this.y, this.w, this.h);
+    this._move();
+  },
+};
+
+const score = {
+  human: 0,
+  computer: 0,
+  increaseHuman: function () {
+    this.human++;
+  },
+  increaseComputer: function () {
+    this.computer++;
+  },
+  draw: function () {
+    canvasCtx.font = "bold 72px Arial";
+    canvasCtx.textAlign = "center";
+    canvasCtx.textBaseline = "top";
+    canvasCtx.fillStyle = "#01341D";
+    canvasCtx.fillText(this.human, field.w / 4, 50);
+    canvasCtx.fillText(this.computer, field.w / 4 + field.w / 2, 50);
+  },
+};
+
+const ball = {
+  x: 0,
+  y: 0,
+  r: 20,
+  speed: 30,
+  directionX: 1,
+  directionY: 1,
+  _calcPosition: function () {
+    // verifica se o jogador 1 fez um ponto (x > largura do campo)
+    if (this.x > field.w - this.r - rightPaddle.w - gapX) {
+      // verifica se a raquete direita está na posição y da bola
+      if (
+        this.y + this.r > rightPaddle.y &&
+        this.y - this.r < rightPaddle.y + rightPaddle.h
+      ) {
+        this._reverseX();
+      } else {
+        score.increaseHuman();
+        this._pointUp();
+      }
+    }
+    // verifica se o jogador 2 fez um ponto (x < 0)
+    if (this.x < this.r + leftPaddle.w + gapX) {
+      if (
+        this.y + this.r > leftPaddle.y &&
+        this.y - this.r < leftPaddle.y + leftPaddle.h
+      ) {
+        //rebate a bola ao inverter o sinal de X
+        this._reverseX();
+      } else {
+        //incrementa a portuação do jogador 1
+        score.increaseComputer();
+        this._pointUp();
+      }
+    }
+
+    // verifica se o jogador 2 fez um ponto (x < 0)
+    if (
+      (this.y - this.r < 0 && this.directionY < 0) ||
+      (this.y > field.h - this.r && this.directionY > 0)
+    ) {
+      this._reverseY();
+    }
+  },
+  _reverseX: function () {
+    this.directionX *= -1;
+  },
+  _reverseY: function () {
+    this.directionY *= -1;
+  },
+  _speedUp: function () {
+    if (this.speed < 100) {
+      this.speed++;
+    }
+  },
+  _pointUp: function () {
+    this._speedUp();
+    this.x = field.w / 2;
+    this.y = field.h / 2;
+  },
+  _move: function () {
+    this.x += this.directionX * this.speed;
+    this.y += this.directionY * this.speed;
+  },
+  draw: function () {
+    canvasCtx.fillStyle = "#ffffff";
+    canvasCtx.beginPath();
+    canvasCtx.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false);
+    canvasCtx.fill();
+
+    this._calcPosition();
+    this._move();
+  },
+};
 
 function setup() {
-  canvasEl.width = canvasCtx.Width = window.innerWidth;
-  canvasEl.height = canvasCtx.height = window.innerHeight;
+  canvasEl.width = canvasCtx.Width = field.w;
+  canvasEl.height = canvasCtx.height = field.h;
 }
 function draw() {
-  canvasCtx.fillStyle = "#286047";
-  /*------------------------desenho do campo---------------------------------------*/
-  canvasCtx.fillRect(0, 0, window.innerWidth, window.innerHeight);
-  //a função .fillRect(x, y, width, height) desenha e define dimensões do retangulo
-
-  /*------------------------desenho da linha central-------------------------------*/
-  canvasCtx.fillStyle = "#ffffff";
-
-  canvasCtx.fillRect(
-    window.innerWidth / 2 - lineWidth / 2,
-    0,
-    lineWidth,
-    window.innerHeight
-  );
-
-  /*------------------------raquetes----------------------------*/
-  //esquerda
-  canvasCtx.fillRect(10, 100, lineWidth, 200);
-
-  //direita
-  canvasCtx.fillRect(window.innerWidth - lineWidth - 10, 200, lineWidth, 200);
-
-  /*---------bolinha--------------*/
-  canvasCtx.beginPath();
-  canvasCtx.arc(500, 300, 20, 0, 2 * Math.PI, false);
-  canvasCtx.fill();
-
-  /*---------placar---------------*/
-  canvasCtx.font = "bold 72px Arial";
-  canvasCtx.textAlign = "center";
-  canvasCtx.textBaseline = "top";
-  canvasCtx.fillStyle = "#01341D";
-  canvasCtx.fillText("3", window.innerWidth / 4, 50);
-  canvasCtx.fillText("5", window.innerWidth / 4 + window.innerWidth / 2, 50);
+  field.draw();
+  line.draw();
+  leftPaddle.draw();
+  rightPaddle.draw();
+  score.draw();
+  ball.draw();
 }
+
 setup();
 draw();
+
+window.animateFrame = (function () {
+  return (
+    window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function (callback) {
+      return window.setTimeout(callback, 1000 / 60);
+    }
+  );
+})();
+
+function main() {
+  animateFrame(main);
+  draw();
+}
+
+setup();
+main();
+canvasEl.addEventListener("mousemove", function (e) {
+  mouse.x = e.pageX;
+  mouse.y = e.pageY;
+});
